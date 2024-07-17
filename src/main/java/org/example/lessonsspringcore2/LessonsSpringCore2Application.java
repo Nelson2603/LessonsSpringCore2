4d7d6efec5dd458e00a9d2afa73d16ab63234145
Nelson2603 <157066286+Nelson2603@users.noreply.github.com>
2024-07-18 01:46:06 +0400

    создал контейнер и вывел объекты

--- src/main/java/org/example/lessonsspringcore2/LessonsSpringCore2Application.java
+++ src/main/java/org/example/lessonsspringcore2/LessonsSpringCore2Application.java
@@ -1,11 +1,21 @@
 package org.example.lessonsspringcore2;
 
+import org.example.lessonsspringcore2.model.Auto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class LessonsSpringCore2Application {
 
     public static void main(String[] args) {
+        //Create container and output object
+        var context = new AnnotationConfigApplicationContext(SpringConfig.class);
+        Auto auto1 = context.getBean("auto1", Auto.class);
+        Auto auto2 = context.getBean("auto2", Auto.class);
+        Auto auto3 = context.getBean("auto3", Auto.class);
+        System.out.println(auto1);
+        System.out.println(auto2);
+        System.out.println(auto3);
     }
 }
